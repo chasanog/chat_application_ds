5a04b5980206171da161f6f31a5cbd018b418c9a
Cihan Hasanoglu <cihanhasanoglu@Cihans-MacBook-Pro.fritz.box>
2021-11-13 14:54:45 +0100

    simply client server setup

---
+++ client.py
@@ -0,0 +1,31 @@
+import socket
+
+# Create a UDP socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Server application IP address and port
+server_address = '127.0.0.1'
+server_port = 10001
+
+# Buffer size
+buffer_size = 1024
+
+# Message sent to server
+message = 'Hi server!'
+address_toConnect = (server_address, server_port)
+client_socket.connect(address_toConnect)
+
+try:
+    # Send data to server
+    client_socket.send(message.encode())
+    print('Sent to server: ', message)
+
+    # Receive response from server
+    print('Waiting for response...')
+    #server_data, server = client_socket.accept()
+    data = client_socket.recv(buffer_size)
+    print('Received message from server: ', data.decode())
+
+finally:
+    client_socket.close()
+    print('Socket closed')
---
+++ server.py
@@ -0,0 +1,34 @@
+import socket
+
+# Create a UDP socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Server application IP address and port
+server_address = '127.0.0.1'
+server_port = 10001
+
+# Buffer size
+buffer_size = 1024
+
+# Message to be sent to client
+message = 'Hi client! Nice to connect with you!'
+
+# Bind socket to port
+server_socket.bind((server_address, server_port))
+server_socket.listen()
+print('Server up and running at {}:{}'.format(server_address, server_port))
+
+while True:
+    print('\nWaiting to receive message...\n')
+
+    # Receive message from client
+    client, address = server_socket.accept()
+    data = client.recv(buffer_size)
+    print("CLIENT ", client)
+    print('Received message from client: ', address)
+    print('Message: ', data.decode())
+
+    if data:
+        # Send message to client
+        client.send(str.encode(message))
+        print(f'Replied to client: ', message)
