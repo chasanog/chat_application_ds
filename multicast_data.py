11a20f0a6bb085cb22160880c37097dde68990ae
Cihan Hasanoglu <cihan.hasanoglu@icloud.com>
2021-12-25 18:32:40 +0100

    added multicast sender and receiver

--- multicast_data.py
+++ multicast_data.py
@@ -3,3 +3,13 @@ MCAST_GRP = '224.1.1.1'
 MCAST_PORT = 5007
 
 MULTICAST_TTL = 2
+
+LEADER = ''
+SERVER_LIST = []
+CLIENT_LIST = []
+
+
+client_running = False
+network_changed = False
+leader_crashed = ''
+replica_crashed = ''
