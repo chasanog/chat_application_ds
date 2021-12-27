de97b175f7f3b63658d99a686fa86b53dfe1ca16
Bujar <bujar94@live.de>
2021-12-27 16:00:39 +0100

    merge multicastserver

--- leader_election.py
+++ leader_election.py
@@ -2,13 +2,36 @@
 Date:       25.12.2021  
 Group:      10
 
-Bully-algorithm - Server leader election
+ring-algorithm - Server leader election
 """
-
 # import Modules
 import socket
-import server
-import Bully
 
 
+def form_ring(members):
+    sorted_binary_ring = sorted([socket.inet_aton(member) for member in members])
+    sorted_ip_ring = [socket.inet_ntoa(node) for node in sorted_binary_ring]
+    return sorted_ip_ring
 
+
+def get_neighbour(members, current_member_ip, direction='left'):
+    current_member_index = members.index(current_member_ip) if current_member_ip in members else -1
+    if current_member_index != -1:
+        if direction == 'left':
+            if current_member_index + 1 == len(members):
+                return members[0]
+            else:
+                return members[current_member_index + 1]
+        else:
+            if current_member_index - 1 == 0:
+                return members[0]
+            else:
+                return members[current_member_index - 1]
+    else:
+        return None
+
+
+def start_leader_election(server_list, leader_server):
+    ring = form_ring(server_list)
+    neighbour = get_neighbour(ring, leader_server, 'right')
+    return neighbour if neighbour != hosts.myIP else None
